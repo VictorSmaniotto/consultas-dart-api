6228119134bcb1c4b673dbebc554f3418e0b48c7
Victor Smaniotto <victorsmaniotto@hotmail.com>
2023-06-27 22:08:28 -0300

    Arquivo retornando Model formatando

--- bin/ProdutoModel.dart
+++ bin/ProdutoModel.dart
@@ -14,18 +14,33 @@ class Produto {
   factory Produto.fromJson(Map<String, dynamic> json) {
     return Produto(
         codigo: json['codigo'],
-        nome: json['nome'],
-        preco: json['preco'],
+        nome: json['descricao'],
+        preco: json['valor'],
         dataAlteracao: DateTime.parse(json['dataAlteracao']));
   }
 }
 
 void main() {
-  Produto produto = Produto(
-      codigo: 1,
-      nome: 'X-TUDO',
-      preco: 40.50,
-      dataAlteracao: DateTime(2023, 6, 27));
+  // Produto produto = Produto(
+  //     codigo: 1,
+  //     nome: 'X-TUDO',
+  //     preco: 40.50,
+  //     dataAlteracao: DateTime(2023, 6, 27));
+
+  // print("Produto:\n"
+  //     "Código: ${produto.codigo}\n"
+  //     "Nome: ${produto.nome}\n"
+  //     "Preço: ${produto.preco}\n"
+  //     "Alteração: ${produto.dataAlteracao}\n");
+
+  Map<String, dynamic> json = {
+    "codigo": 2,
+    "descricao": "X-Bacon",
+    "valor": 19.90,
+    "dataAlteracao": "2023-06-27 22:01"
+  };
+
+  Produto produto = Produto.fromJson(json);
 
   print("Produto:\n"
       "Código: ${produto.codigo}\n"
